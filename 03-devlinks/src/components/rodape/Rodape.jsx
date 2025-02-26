9a76450f741775078c939f50ad464950d1945368
Beatriz <beatriz080407@gmail.com>
2025-02-26 08:04:29 -0300

    PrimeiroReact

---
+++ 03-devlinks/src/components/rodape/Rodape.jsx
@@ -0,0 +1,13 @@
+import styles from './Rodape.module.css'
+
+const Rodape = () => {
+  return (
+    <footer>
+        <p>
+            feito com ❤ por <a href="https://github.com">Beatriz Silva</a>
+        </p>
+    </footer>
+  );
+};
+
+export default Rodape;
