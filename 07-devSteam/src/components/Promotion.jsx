632748ac538767fe4e7bdb18c4c8f8de7f04e782
Beatriz <beatriz080407@gmail.com>
2025-04-22 14:23:18 -0300

    design do promo card

--- 07-devSteam/src/components/Promotion.jsx
+++ 07-devSteam/src/components/Promotion.jsx
@@ -3,10 +3,13 @@ import PromoCard from './PromoCard'
 
 const Promotion = () => {
   return (
-    <div id='promotion'>
-        <h2>Promoções</h2>
-        <div id='itensPromo'>
+    <div id='promotion' className='container w-50 my-5'>
+        <h2 className='text-uppercase'>Promoções</h2>
+        <div id='itensPromo'
+         className='d-flex flex-wrap gap-4 justify-content-between'>
           {/* inserir os cards de promoção*/}
+          <PromoCard/>
+          <PromoCard/>
           <PromoCard/>
         </div>
     </div>
