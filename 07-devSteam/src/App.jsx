2db8eb140b1b47fe29b166bafb61b56549464455
Beatriz <beatriz080407@gmail.com>
2025-04-22 11:27:12 -0300

    ajustes nos cards de promotion

--- 07-devSteam/src/App.jsx
+++ 07-devSteam/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Header from './components/Header'
+import Promotion from './components/Promotion'
 
 function App() {
 
   return (
     <>
      <Header />
+     <Promotion />
     </>
   )
 }
