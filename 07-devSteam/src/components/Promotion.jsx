14cc8d7cf2ed09f3008b7cff7c79edf0fed8cbcf
Beatriz <beatriz080407@gmail.com>
2025-04-23 10:24:33 -0300

    fazendo o adicionando ao carrinho

--- 07-devSteam/src/components/Promotion.jsx
+++ 07-devSteam/src/components/Promotion.jsx
@@ -36,36 +36,52 @@ const Promotion = (props) => {
         "https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg",
     },
     {
-        id: 3,
-        titulo: "Elden Ring",
-        preco: 249.9,
-        desconto: 35, // 35% off
-        imagem:
-          "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/header.jpg",
-      },
-      {
-        id: 4,
-        titulo: "Red Dead Redemption 2",
-        preco: 199.9,
-        desconto: 40, // 40% off
-        imagem:
-          "https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg",
-      },
+      id: 5,
+      titulo: "Counter-Strike 2",
+      preco: 0.0,
+      desconto: 0,
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg",
+    },
+    {
+      id: 6,
+      titulo: "Cyberpunk 2077",
+      preco: 129.99,
+      desconto: 20, // 20% off
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg",
+    },
+    {
+      id: 7,
+      titulo: "Elden Ring",
+      preco: 249.9,
+      desconto: 35, // 35% off
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/header.jpg",
+    },
+    {
+      id: 8,
+      titulo: "Red Dead Redemption 2",
+      preco: 199.9,
+      desconto: 40, // 40% off
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg",
+    },
   ];
 
   return (
-    <div id="promotion" className="container w-75 my-5">
-      <h2 className="text-uppercase">Promoções</h2>
+    <div id="promotion" className="container w-75 my-4">
+      <h2 className="text-uppercase text-center text-md-start ms-md-5 ps-md-3 mb-4">Promoções</h2>
       <div
         id="itensPromo"
-        className="d-flex flex-wrap gap-4 justify-content-between"
+        className="d-flex flex-wrap gap-4 justify-content-around"
       >
-        {/* Filtrando e mapeando o array */}
+        {/* mapeando um array com react */}
         {games
-          .filter((jogo) => jogo.desconto > 0) // Exibe apenas jogos com desconto
-          .sort((a, b) => b.desconto - a.desconto) // Ordena por maior desconto
-          // .sort(() => Math.random() - 0.5) // Embaralha os jogos
-          .slice(0, 3) // Limita a 3 jogos
+          .filter((jogo) => jogo.desconto > 0)
+          //.sort((a, b) => b.desconto - a.desconto) //ordenação por desconto decrescente
+          .sort(() => Math.random() - 0.5) //ordenação aleatória
+          .slice(0, 3)
           .map((jogo) => (
             <PromoCard
               key={jogo.id}
@@ -73,7 +89,9 @@ const Promotion = (props) => {
               preco={jogo.preco.toFixed(2)}
               desconto={jogo.desconto}
               imagem={jogo.imagem}
-              onAddCarinho={props.onAddCarinho} // Passando a função de adicionar ao carrinho
+              //adicionando a opção de click com os itens do jogo no carrinho
+              onAddCarrinho={() => props.onAddCarrinho(jogo)}
+              //callback para adicionar somente um item e não todos os itens do array
             />
           ))}
       </div>
@@ -82,4 +100,3 @@ const Promotion = (props) => {
 };
 
 export default Promotion;
-
